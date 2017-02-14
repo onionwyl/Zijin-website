7a3f087814b5244914e73b2f126919f29744864d
onionwyl <362919284@qq.com>
2017-02-14 22:06:02 +0800

    Init homepage

---
+++ resources/views/home/index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>子衿之家 - 上网导航第一站</title>
+	<meta name="description" content="子衿之家是子衿集团旗下门户网站，提供网址导航、常用功能、新闻推送等服务，子网站包括子衿视频、子衿音乐、子衿应用中心、子衿游戏、子衿小窝、子衿邮箱和林园论坛等服务内容" />
+	<meta name="keywords" content="子衿集团，渊渊子衿，金禄渊，子衿之家" />
+	<link rel="shortcut icon" href="media/icongreen1.png" />	
+	<link rel="stylesheet" type="text/css" href="css/mystyle.css" />
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css" />
+	<meta property="qc:admins" content="7472115167615161516212166375" />
+
+	<script>
+		var _hmt = _hmt || [];
+		(function() {
+		var hm = document.createElement("script");
+		hm.src = "//hm.baidu.com/hm.js?14fb61bda8dcbd4eb59b36370c714a3f";
+		var s = document.getElementsByTagName("script")[0]; 
+		s.parentNode.insertBefore(hm, s);
+		})();
+	</script>
+	
+
+</head>
+
+<body>
+<div class="header">
+	<div class="daohanglanwhole">
+		<div class="daohanglan">
+			<div id="scroll"></div>
+			<a href="//www.yuanyuanzijin.com" target="_parent" class="daohangactive">首页</a>
+			<a href="//v.yuanyuanzijin.com" target="_blank">视频</a>
+			<!--<a href="#" target="_parent">音乐</a>
+			<a href="#" target="_parent">应用</a>
+			<a href="#" target="_parent">游戏</a>-->
+			<a href="//mail.yuanyuanzijin.com" target="_blank">邮箱</a>
+			<a href="//works.yuanyuanzijin.com" target="_blank">展馆</a>
+			<a href="//blog.yuanyuanzijin.com" target="_blank">小窝</a>
+			<a href="//www.yuanyuanzijin.com/web-cloud/test" target="_blank">云盘测试</a>
+			<a href="#" target="_parent">更多</a>
+		</div>
+		<div class="setfirstpage">
+			<a>喜欢的话就收藏吧</a>
+		</div>
+	</div>
+
+
+</div>
+
+
+<div class="nav">
+	<div class="nav-1">
+		<div class="animated lightSpeedIn">
+			<img class="logo" src="media/logowww.png" />
+		</div>
+	</div>
+	<div class="nav-2">
+<!--		<iframe name="weather_inc" src="http://i.tianqi.com/index.php?c=code&id=1" width="500" height="35" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+-->	</div>
+</div>
+
+<div class="middle">
+	
+	<div class="left">
+		<div class="shang">
+			<div class="animated slideInDown">
+				<form onsubmit="return baiduWithHttps(this)" action="http://www.baidu.com/baidu" target="_blank" >
+	    			<input name="tn" type="hidden" value="SE_zzsearchcode_shhzc78w">
+	    			<input class="search" type="text"  onfocus="checkHttps" name="word"  size="30" baiduSug="2" autofocus="autofocus">
+	    			<input type="submit" value="百度一下" style="width:100px; height:32px; background-color: dodgerblue; border: none; color: white; font-size: 16px;">
+				</form>
+			</div>
+		</div>
+		
+		<div class="xia">
+			<div class="xia0">
+				<div class="xia0-header">
+					<b>常用</b>
+				</div>
+				<ul class="weblist">
+					<li><a href="//qq.com" target="_blank">腾讯网</a></li>
+					<li><a href="//baidu.com" target="_blank">百 度</a></li>
+					<li><a href="//163.com" target="_blank">网 易</a></li>
+					<li><a href="//sina.com.cn" target="_blank">新 浪</a></li>
+				</ul>
+			</div>
+			<div class="xia0">
+				<div class="xia0-header">
+					<b>视频</b>
+				</div>
+				<ul class="weblist">
+					<li><a href="//youku.com" target="_blank">优 酷</a></li>
+					<li><a href="//le.com" target="_blank">乐视视频</a></li>
+					<li><a href="//v.qq.com" target="_blank">腾讯视频</a></li>
+					<li><a href="//iqiyi.com" target="_blank">爱奇艺</a></li>
+				</ul>
+			</div>
+			<div class="xia0">
+				<div class="xia0-header">
+					<b>音乐</b>
+				</div>
+				<ul class="weblist">
+					<li><a href="//y.qq.com" target="_blank">QQ音乐</a></li>
+					<li><a href="//music.163.com" target="_blank">网易云音乐</a></li>
+					<li><a href="//kugou.com" target="_blank">酷狗音乐</a></li>
+					<li><a href="//music.baidu.com" target="_blank">百度音乐</a></li>
+				</ul>				
+			</div>
+			<div class="xia0">
+				<div class="xia0-header">
+					<b>邮箱</b>
+				</div>
+				<ul class="weblist">
+					<li><a href="//mail.163.com" target="_blank">网易邮箱</a></li>
+					<li><a href="//mail.qq.com" target="_blank">QQ邮箱</a></li>
+					<li><a href="//mail.sina.com.cn" target="_blank">新浪邮箱</a></li>
+					<li><a href="//gmail.com" target="_blank">Gmail</a></li>
+				</ul>					
+			</div>
+			<div class="xia0">
+				<div class="xia0-header">
+					<b>购物</b>
+				</div>				
+				<ul class="weblist">
+					<li><a href="//taobao.com" target="_blank">淘宝网</a></li>
+					<li><a href="//jd.com" target="_blank">京 东</a></li>				
+					<li><a href="//vip.com" target="_blank">唯品会</a></li>				
+					<li><a href="//yhd.com" target="_blank">1号店</a></li>
+				</ul>
+			</div>
+			<div class="xia0">
+				<div class="xia0-header">
+					<b>社交</b>
+				</div>				
+				<ul class="weblist">
+					<li><a href="//weibo.com" target="_blank">微 博</a></li>				
+					<li><a href="//wx.qq.com" target="_blank">微 信</a></li>
+					<li><a href="//qzone.qq.com" target="_blank">QQ空间</a></li>
+					<li><a href="//tieba.baidu.com" target="_blank">百度贴吧</a></li>				
+				</ul>
+			</div>
+		</div>
+	</div>
+		
+		
+	<div class="animated bounceIn">
+		<div class="right">
+		
+			<div class="right-shang">
+				
+				<?php
+				//检测是否登录，若没登录显示登录按钮
+				if(!isset($_SESSION['userid'])){
+					echo ("<br /><div>登录后可以尊享会员服务</div>
+						<a href='/php/passportlogin.php'><button class='loginbutton'>立即登录</button></a>
+					");
+				}
+				//包含数据库连接文件
+				else{
+				include('/php/conn.php');
+				$userid = $_SESSION['userid'];
+	//			$username = $_SESSION['username'];
+				$user_query = mysqli_query($conn,"select * from user where id=$userid limit 1");
+				$row = mysqli_fetch_array($user_query);
+				$username = $row['username'];
+				include('php/my/level.php');
+				echo("<div class='portrait'><img style='width:70px;height:70px;' src='".$row['portraitaddr']."' /></div>
+					<div class='username'>".$username."<br />
+					等级：".$level."<br />
+					积分：".$row['points']."</div>
+					<br />
+					<a href='/php/login.php?action=logout'>退出</a>
+					<a href='/php/my.php' target='_blank'><button class='vipbutton'>个人中心</button></a><br />"
+				);
+				}
+				//include('php/mail/index.php');
+				?>
+			</div>
+		
+			<div class="right-xia">
+				<div class="loading" id="loading">
+					<div class="spinner">
+					  <div class="rect1"></div>
+					  <div class="rect2"></div>
+					  <div class="rect3"></div>
+					  <div class="rect4"></div>
+					  <div class="rect5"></div>
+					</div> 
+				</div> 
+				<iframe id="mailiframe" class="mailiframe" src="php/mail/index.php" scrolling="no"></iframe>
+				
+					<script language="JavaScript"> 
+					 var iframe = document.getElementById("mailiframe"); 
+					// document.getElementById("loading").style.display="block"; 
+					 if (iframe.attachEvent){  
+					    iframe.attachEvent("onload", function(){ 
+					    document.getElementById("loading").style.display="none";
+					    document.getElementById("mailiframe").style.display="block"; 
+					    });  
+					 } else {
+					    iframe.onload = function(){
+					    document.getElementById("loading").style.display="none"; 
+					    document.getElementById("mailiframe").style.display="block"; 
+					     };  
+					 }  
+					 </script> 
+				
+				<a href="/php/book.php" target="_parent"><button class="bookbutton">留言板</button></a>
+				<a href="history.php" target="_blank"><button class="historybutton">网站创作历程</button></a>
+			</div>
+		</div>
+	</div>
+		
+	<div class="clear"></div>
+	
+	</div>
+	
+	<div class="middle2">
+			<div class="news-header">
+				最新新闻
+			</div>
+			<div class="news">
+				<span class="news-smallheader">&nbsp;&nbsp;<b>国内要闻</b></span>
+				<script language="JavaScript" type="text/JavaScript" src="http://news.baidu.com/n?cmd=1&class=civilnews&pn=1&tn=newsbrofcu"></script>
+			</div>
+			<div class="news">
+				<span class="news-smallheader">&nbsp;&nbsp;<b>国际要闻</b></span>
+				<script language="JavaScript" type="text/JavaScript" src="http://news.baidu.com/n?cmd=1&class=internews&pn=1&tn=newsbrofcu"></script>
+			</div>
+	</div>
+
+	<div class="middle3">
+		<div class="news-header">
+			智慧大工
+		</div>
+		<!--<div class="content">
+			<iframe name="main" width="500" height="210" id="main" src="http://202.118.64.15/fabu/cardUserManager.do?method=queryTboxCount" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+			<p>信息来源：校园一卡通系统，由于存衣柜不全开，结果仅供参考，建议已使用数量小于100时前往</p>
+		</div>-->
+	</div>
+	
+	<div class="middle3">
+		更多内容，敬请期待
+	</div>
+	
+<div class="footer">
+	<div class="linkbottom">
+		<a href="//www.yuanyuanzijin.com/" target="_blank">子衿之家</a> |
+		<a href="//v.yuanyuanzijin.com/" target="_blank">子衿视频</a> |
+		<a href="//m.yuanyuanzijin.com/" target="_blank">子衿音乐</a> |
+		<a href="//app.yuanyuanzijin.com/" target="_blank">子衿应用商店</a> |
+		<a href="//game.yuanyuanzijin.com/" target="_blank">子衿游戏</a> |
+		<a href="//mail.yuanyuanzijin.com/" target="_blank">子衿邮箱</a> |
+		<a href="//blog.yuanyuanzijin.com/" target="_blank">子衿小窝</a> |
+		<a href="//lunyuan.club/" target="_blank">林园社区</a>
+
+	</div>
+	<div class="beian">
+		<a href="//www.miibeian.gov.cn" target="_blank">辽ICP备16006372号</a>
+		<a href="//www.jinluyuan.xyz" target="_blank">金禄渊
+		</a><br />
+		访问总量：<?php //include('D://wwwroot/php/totalvisited.php') ?>次<br />
+		&copy;Copyright 2014-<?php echo date("Y")?> Zijin Technology All Rights Reserved.
+	</div>
+</div>
+
+
+
+<script src="//s1.bdstatic.com/r/www/cache/global/js/BaiduHttps_20150714_zhanzhang.js"></script>
+<script>
+    function checkHttps () {
+        BaiduHttps.useHttps();    
+    };
+    function baiduWithHttps (formname) {
+        var data = BaiduHttps.useHttps();
+        if (data.s === 0) {
+            return true;
+        }
+        else {
+            formname.action = 'https://www.baidu.com/baidu' + '?ssl_s=1&ssl_c' + data.ssl_code;
+            return true;
+        }
+    };
+    
+    $(window).scroll(function() {  
+        var yheight1=window.pageYOffset; //滚动条距顶端的距离  
+        var yheight=getScrollTop(); //滚动条距顶端的距离  
+        if(yheight >= "80"){
+        	document.getElementById("scroll").innerHTML = "<img src='media/zjlogo.png'/>";
+		}else{
+			document.getElementById("scroll").innerHTML = "";
+		}
+    });  
+    
+    function getScrollTop() {  
+        var scrollPos;  
+        if (window.pageYOffset) {  
+        scrollPos = window.pageYOffset; }  
+        else if (document.compatMode && document.compatMode != 'BackCompat')  
+        { scrollPos = document.documentElement.scrollTop; }  
+        else if (document.body) { scrollPos = document.body.scrollTop; }   
+        return scrollPos;   
+}
+
+///校园浴池
+// 	function iFrameHeight() {   
+//		var ifm= document.getElementById("main");   
+//		var subWeb = document.frames ? document.frames["main"].document : ifm.contentDocument;   
+//		if(ifm != null && subWeb != null) {
+// 			ifm.height = subWeb.body.scrollHeight;
+//		}   
+//	}   
+</script>
+
+<script charset="gbk" src="http://www.baidu.com/js/opensug.js"></script>
+<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery/jquery-1.4.min.js"></script>
+
+
+</body>
+</html>
